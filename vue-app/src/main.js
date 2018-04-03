63e920c429e3e390a9fa64c62f6c196a978997ea
Markus Danek <markus.danek-cic@ibm.com>
2018-04-03 11:33:51 +0200

    Added vue-analytics with basic functionality

--- vue-app/src/main.js
+++ vue-app/src/main.js
@@ -1,7 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueAnalytics from 'vue-analytics'
 
 Vue.config.productionTip = false
+
+Vue.use(VueAnalytics, {
+  id: 'UA-54514383-9',
+  debug: {
+    enabled: true,
+    trace: true,
+    sendHitTask: true
+  },
+  fields: {
+    cookieDomain: 'none'
+  }
+})
 
 new Vue({
   render: h => h(App)
