c587fe805e5840fc7e5931a8f171f64fe1195ae0
Markus Danek <markus.danek-cic@ibm.com>
2018-04-17 10:17:07 +0200

    Add demo Vue.js project

---
+++ demo-app/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <CookieMessage />
+    <img src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import CookieMessage from './components/CookieMessage.vue'
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld,
+    CookieMessage
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ demo-app/src/main.js
@@ -0,0 +1,33 @@
+import Vue from 'vue'
+import App from './App.vue'
+import VueAnalytics from 'vue-analytics'
+
+const getGDPR = localStorage.getItem('GDPR:accepted');
+
+if (typeof getGDPR !== 'undefined' && getGDPR === 'true' ) {
+  Vue.use(VueAnalytics, {
+    id: 'UA-54514383-9',
+    disabled: false,
+    debug: {
+      enabled: true,
+      trace: true,
+      sendHitTask: true
+    }
+  })
+} else if (typeof getGDPR !== 'undefined' && getGDPR === 'false'){
+  Vue.use(VueAnalytics, {
+    id: 'UA-54514383-9',
+    disabled: true,
+    debug: {
+      enabled: true,
+      trace: true,
+      sendHitTask: true
+    }
+  })
+}
+
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App)
+}).$mount('#app')
